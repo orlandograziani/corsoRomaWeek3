72441c9535398ac5095b4acf6e58c29528929b10
sorrentmutie <salvatore.sorrentino@live.com>
2018-06-10 10:22:36 +0200

    pubblicazione su github

---
+++ ProvaSuAzure/ProvaSuAzure/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProvaSuAzure.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
