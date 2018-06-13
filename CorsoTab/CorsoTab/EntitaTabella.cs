ec500a9c88b3f42e35b97e1c51ca0eb13a9d0783
Lorenzo Magi <lorenzomagi@gmail.com>
2018-06-13 17:40:56 +0200

    esercitazione tabelle azure

---
+++ CorsoTab/CorsoTab/EntitaTabella.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorsoTab
+{
+    public class EntitaTabella: TableEntity
+    {
+        public EntitaTabella(string lastName, string firstName)
+        {
+            this.PartitionKey = lastName;
+            this.RowKey = firstName;
+        }
+
+        public EntitaTabella() { }
+
+        public string nome { get; set; }
+
+        public string cognome { get; set; }
+    }
+}
---
+++ CorsoTab/CorsoTab/Program.cs
@@ -0,0 +1,43 @@
+﻿
+
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorsoTab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // Parse the connection string and return a reference to the storage account.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(
+                CloudConfigurationManager.GetSetting("StorageConnectionString"));
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+
+            // Create the CloudTable object that represents the "people" table.
+           CloudTable table = tableClient.GetTableReference("TestTabella1");
+
+
+
+            // Construct the query operation for all customer entities where PartitionKey="Smith".
+            TableQuery<EntitaTabella> query = new TableQuery<EntitaTabella>().Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, "testPartvalue"));
+
+            // Print the fields for each customer.
+            foreach (EntitaTabella entity in table.ExecuteQuery(query))
+            {
+                Console.WriteLine("{0}, {1}\t{2}\t{3}", entity.PartitionKey, entity.RowKey,
+                    entity.nome, entity.cognome);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
